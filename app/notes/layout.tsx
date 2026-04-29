42788550873622f956d458bd37865836f82a0ccc
I.Kumpan <ilyakumpan11@gmail.com>
2026-04-29 22:56:27 +0300

    fix-01.4

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,14 +1,16 @@
-export default function NotesLayout({
-  children,
-  modal, 
-}: {
+import React from 'react';
+
+interface NotesLayoutProps {
   children: React.ReactNode;
-  modal: React.ReactNode;
-}) {
+  modal: React.ReactNode; 
+}
+
+export default function NotesLayout({ children, modal }: NotesLayoutProps) {
   return (
     <>
       {children}
-      {modal} 
+      
+      {modal}
     </>
   );
 }
