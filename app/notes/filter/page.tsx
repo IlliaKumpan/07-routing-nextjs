a149d6cf9930a5ad85326c64cefe8f0b6a99220c
I.Kumpan <ilyakumpan11@gmail.com>
2026-05-02 23:38:59 +0300

    fix-2

--- app/notes/filter/page.tsx
+++ app/notes/filter/page.tsx
@@ -1,6 +1,5 @@
 import FilteredNotesPage from './[...tag]/page';
 
 export default async function DefaultFilterPage() {
-  // Викликаємо ту саму логіку, передаючи 'all' як значення за замовчуванням
-  return <FilteredNotesPage params={{ tag: ['all'] }} />;
+  return <FilteredNotesPage params={Promise.resolve({ tag: ['all'] })} />;
 }
--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
-
 interface NotesLayoutProps {
   children: React.ReactNode;
-  modal: React.ReactNode; 
+  modal?: React.ReactNode;
 }
 
 export default function NotesLayout({ children, modal }: NotesLayoutProps) {
   return (
     <>
       {children}
-      
-      {modal}
+      {modal} 
     </>
   );
 }
