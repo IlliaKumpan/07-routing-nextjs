601b85a45682af33206d897274fcacb0ca2a15d2
I.Kumpan <ilyakumpan11@gmail.com>
2026-05-03 20:17:24 +0300

    new-fix-2

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,27 +1,27 @@
-import { Modal } from '@/components/Modal/Modal';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { fetchNoteById } from '@/lib/api';
 import NotePreviewClient from '../../@modal/(.)notes/[id]/NotePreview.client';
+import { Modal } from '@/components/Modal/Modal';
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
-async function getNoteById(id: string) {
-  const res = await fetch(`https://notehub-public.goit.study/api/notes/${id}`, {
-    cache: 'no-store',
-  });
-  if (!res.ok) return null;
-  return res.json();
-}
-
-export default async function NoteModalPage({ params }: Props) {
+export default async function NotePage({ params }: Props) {
   const { id } = await params;
-  const note = await getNoteById(id);
+  const queryClient = new QueryClient();
 
-  if (!note) return null;
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
 
   return (
     <Modal>
-      <NotePreviewClient note={note} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NotePreviewClient id={id} />
+      </HydrationBoundary>
     </Modal>
   );
 }
